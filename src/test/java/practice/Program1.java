9a8848303cd4da41c98802830e1edde3b024c2b4
Pravee <Pravee@Praveen>
2022-12-11 21:43:07 +0530

    static and non-static accesing

---
+++ src/test/java/practice/Program1.java
@@ -0,0 +1,36 @@
+package practice;
+
+public class Program1  {
+	static int ha=10;
+	int b=20;
+	
+	static public void running()
+	{
+		System.out.println("This is static same class");
+	}
+	
+	public void practice()
+	{
+		System.out.println("isit working....");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		Program1 p=new Program1();
+		Program2 p2=new Program2();
+		System.out.println(ha);
+		System.out.println(p.b);
+		running();
+		p.practice();
+		
+		System.out.println(p2.c);
+		System.out.println(p2.d);
+		p2.runthe();
+		p2.anotherclass();
+		
+		
+	}
+
+}
---
+++ src/test/java/practice/Program2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Program2 {
+	int e=50;
+	static int c=25;
+	int d=30;
+	static public void anotherclass()
+	{
+		System.out.println("static different class");
+	}
+	public void runthe()
+	{
+		System.out.println("not static different class");
+	}
+}
